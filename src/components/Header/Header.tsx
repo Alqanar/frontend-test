932b79e13a4dfcaa8f4a9453871faa7f9e748534
Katrin Shibakova <katrina.gubanova@gmail.com>
2021-04-22 16:11:53 +0300

    added styles for the search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from 'react';
+import { Search } from '../Search/Search';
 import styles from './Header.module.scss';
 
 interface IHeaderProps {
   title: string;
-  children?: React.ReactNode;
 }
 
-export const Header: FC<IHeaderProps> = function Header({ title, children }) {
+export const Header: FC<IHeaderProps> = function Header({ title }) {
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
         <h1 className={styles.title}>{title}</h1>
-        {children}
+
+        <Search />
       </div>
     </header>
   );
